b888ddd84902fd4adf06f4c3a27489359e614880
michael <michael@foruteam.net>
2020-02-03 13:54:11 +0800

    修正port

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import VueSocketIO from 'vue-socket.io'
 
 Vue.use(new VueSocketIO({
   debug: false,
-  connection: 'http://localhost:3000',
+  connection: 'http://localhost:3021',
   vuex: {
     store,
     actionPrefix: 'socket_',
